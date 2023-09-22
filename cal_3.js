aaf83a7a8c1876f63fd126d4b9c9e59c66eb7a55
kimsohyeon <aotoyae@gmail.com>
2023-09-22 23:15:16 +0900

    third commit

--- cal_3.js
+++ cal_3.js
@@ -1,68 +1,65 @@
-const $steps = document.querySelector("#steps");
-const $answer = document.querySelector("#answer");
-const $btn = document.querySelector("#btn");
+const steps = document.querySelector("#steps");
+const answer = document.querySelector("#answer");
+const btn = document.querySelector("#btn");
+// const plusMinus = document.querySelector(".plusMinus")
 
 const data = {
   prev: "",
   curr: "",
   operator: undefined,
-  pressdResult: false,
+  pressedResult: false,
 };
 
-$btn.addEventListener("click", (e) => {
+btn.addEventListener("click", (e) => {
   const target = e.target;
-  if (target.tagName !== "BUTTON") {
-    return;
-  }
-  if (target.id == "reset") {
-    reset_data();
+  if (target.id === "reset") {
+    resetData();
     return;
   }
   if (target.classList.contains("num")) {
-    on_num(data.operator, target);
+    onNum(data.operator, target);
   }
   if (target.classList.contains("op")) {
-    on_op(target);
+    onOp(target);
   }
-  if (target.id == "btnResult") {
-    show_result();
+  if (target.id === "btnResult") {
+    showResult();
   }
-  target.blur();
 });
-function on_num(bool, target) {
-  const val = target.dataset.val;
-  const prevOrcurr = bool ? "curr" : "prev";
-  if (val == "-1") {
-    data[prevOrcurr] = Number(data[prevOrcurr]) * -1;
+function onNum(bool, target) {
+  const val = target.value;
+  const prevOrCurr = bool ? "curr" : "prev";
+  if (data[prevOrCurr] === -0){
+    console.log("hi")
+    answer.innerText = data[prevOrCurr];
+  }
+  if (val === "-1") {
+    // console.log(typeof data[prevOrCurr])
+    data[prevOrCurr] = Number(data[prevOrCurr]) * -1;
+    console.log("bye")
+
   } else {
-    data[prevOrcurr] += val;
+    data[prevOrCurr] += val;
   }
-  $answer.textContent = data[prevOrcurr];
+  answer.innerText = data[prevOrCurr];
 }
-function on_op(target) {
-  $steps.classList.remove("off");
-  const val_op = target.dataset.val;
-  data.operator = val_op;
-  if (data.prev == undefined) {
-    return;
+function onOp(target) {
+  steps.classList.remove("off");
+  data.operator = target.value;
+  if (!data.pressedResult && data.curr) {
+    showResult();
   }
-  if (!data.pressdResult && data.curr) {
-    show_result();
-  }
-  show_middleStep();
+  showMiddleStep();
   data.curr = "";
-  data.pressdResult = false;
+  data.pressedResult = false;
 }
-function show_result() {
-  if (data.prev == undefined || data.curr == undefined || !data.operator) {
-    return;
-  }
-  data.pressdResult = true;
-  show_finalStep();
-  data.prev = calculSwitch();
-  $answer.textContent = data.prev;
+function showResult() {
+  data.pressedResult = true;
+  showFinalStep();
+  data.prev = calSwitch();
+  answer.innerText = data.prev;
 }
-function calculSwitch() {
+function calSwitch() {
   const { prev, curr, operator } = data;
   switch (operator) {
     case "+":
@@ -77,7 +74,7 @@ function calculSwitch() {
       return Number(prev) ** Number(curr);
   }
 }
-function operator_to_string() {
+function opToString() {
   const { operator } = data;
   switch (operator) {
     case "+":
@@ -92,20 +89,18 @@ function operator_to_string() {
       return "**";
   }
 }
-function show_middleStep() {
-  const step_str = `${data.prev} ${operator_to_string()}`;
-  $steps.textContent = step_str;
+function showMiddleStep() {
+  steps.innerText = `${data.prev} ${opToString()}`;
 }
-function show_finalStep() {
-  const calcul_str = `${data.prev} ${operator_to_string()} ${data.curr}`;
-  $steps.textContent = `${calcul_str} =`;
+function showFinalStep() {
+  steps.innerText = `${data.prev} ${opToString()} ${data.curr} =`;
 }
-function reset_data() {
+function resetData() {
   data.prev = "";
   data.curr = "";
-  $steps.textContent = "&nbsp";
-  $steps.classList.add("off");
-  $answer.textContent = "0";
+  steps.innerText = "&nbsp";
+  steps.classList.add("off");
+  answer.innerText = "0";
   data.operator = undefined;
-  data.pressdResult = true;
+  data.pressedResult = true;
 }
