ad04ad830712645f3a60c2a610704efdab100e6a
kimsohyeon <aotoyae@gmail.com>
2023-10-15 21:45:13 +0900

    localStorage 수정

--- legal_dong/js/login.js
+++ legal_dong/js/login.js
@@ -3,29 +3,29 @@ const passBox = document.getElementsByClassName("user-pass");
 const joinBtn = document.getElementsByClassName("join-btn");
 const loginBtn = document.getElementsByClassName("login-btn");
 
-// function onJoinSubmit(event) {
-//   event.preventDefault();
-//   const userid = idBox.value;
-//   localStorage.setItem(USERID_KEY, userid);
-// }
+localStorage.setItem("users", JSON.stringify([]));
+const users = JSON.parse(localStorage.getItem("users"));
 
 function handleSubmitId(event) {
-  const ID = idBox[0].value;
-  const PASSWORD = passBox[0].value;
   event.preventDefault();
-  console.log("hi");
-  localStorage.setItem(ID, PASSWORD);
+  const id = idBox[0].value;
+  const password = passBox[0].value;
+  users.push({ id: id, password: password });
+  localStorage.setItem("users", JSON.stringify(users));
+  alert(`${id}님 회원가입을 환영합니다.`);
 }
 
 function checkValue(event) {
   event.preventDefault();
-  const ID = idBox[0].value;
-  const PASSWORD = passBox[0].value;
-  if (localStorage.getItem(ID) == PASSWORD) {
-    alert(`${ID}님 반갑습니다.`);
-    window.location.href = "sub.html";
-  } else {
-    alert(`아이디 또는 비밀번호를 잘못 입력했습니다.`);
+  const id = idBox[0].value;
+  const password = passBox[0].value;
+  for (i = 0; i < users.length; i++) {
+    if (users[i].id == id && users[i].password == password) {
+      alert(`${id}님 반갑습니다.`);
+      window.location.href = "sub.html";
+    } else {
+      alert(`아이디 또는 비밀번호를 잘못 입력했습니다.`);
+    }
   }
 }
 
