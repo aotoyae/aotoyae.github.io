ded41f6c550a9c51dcc1bbe4bcfbb5e763e648a9
kimsohyeon <aotoyae@gmail.com>
2023-10-16 23:29:38 +0900

    key 수정

--- legal_dong/js/api.js
+++ legal_dong/js/api.js
@@ -8,8 +8,10 @@ const creatDate = document.querySelector("#container tr .creat-date");
 const deleteDate = document.querySelector("#container tr .delete-date");
 const pastCode = document.querySelector("#container tr .past-code");
 
+const key = api.key;
+
 fetch(
-  "https://api.odcloud.kr/api/15063424/v1/uddi:257e1510-0eeb-44de-8883-8295c94dadf7?page=1&perPage=10&&serviceKey=1fQnx228HxXx%2FibxiDlyQJH%2FJtwMiCBQ8BI9iT0unNG9gvlFKGnW3QFDt7I8SotDg%2FtJxFAJTXi712a220uc7w%3D%3D  "
+  `https://api.odcloud.kr/api/15063424/v1/uddi:257e1510-0eeb-44de-8883-8295c94dadf7?page=1&perPage=10&&serviceKey=${key}`
 )
   .then((response) => response.json())
   .then((data) => console.log(data));
