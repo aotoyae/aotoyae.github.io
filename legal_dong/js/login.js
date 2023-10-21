1885fd7891f9db01ca28881d47e015349ea24485
kimsohyeon <aotoyae@gmail.com>
2023-10-21 21:49:09 +0900

    유효성 검사

--- legal_dong/js/login.js
+++ legal_dong/js/login.js
@@ -11,24 +11,24 @@ function handleSubmitId(event) {
   const id = idBox[0].value;
   const password = passBox[0].value;
 
-  let idRule = /^[a-z0-9]{5,20}$/;
-  let passRule = /^[a-zA-Z0-9]{8,20}$/;
+  const idRule = /^[a-z0-9]{5,20}$/;
+  const passRule = /^[a-zA-Z0-9]{8,20}$/;
+
   if (id === "") {
     alert(`아이디를 입력하세요.`);
     return false;
   } else if (!idRule.test(id)) {
-    alert(`5~20자의 영문 소문자, 숫자만 사용 가능합니다.`);
+    alert(`아이디: 5~20자의 영문 소문자, 숫자만 사용 가능합니다.`);
     return false();
   }
   if (password === "") {
     alert(`비밀번호를 입력하세요.`);
     return false;
   } else if (!passRule.test(password)) {
-    alert(`8~20자의 영문 대소문자, 숫자만 사용 가능합니다.`);
+    alert(`비밀번호: 8~20자의 영문 대소문자, 숫자만 사용 가능합니다.`); // 대문자 실행 안됨
     return false;
   }
   for (i = 0; i < users.length; i++) {
-    if (users !== "") console.log(users[i].id);
     if (id === users[i].id) {
       alert(`사용할 수 없는 아이디입니다. 다른 아이디를 입력해 주세요.`);
       return false;
@@ -56,12 +56,18 @@ function checkValue(event) {
       continue;
     } else if (users[i].id == id && users[i].password == password) {
       alert(`${id}님 반갑습니다.`);
-      window.location.href = "sub.html";
+      // window.location.href = "sub.html";
+    } else {
+      alert(`아이디 또는 비밀번호를 잘못 입력했습니다.`);
     }
-    // else if (users[i].id !== id && users[i].password !== password) {
-    //   alert(`아이디 또는 비밀번호를 잘못 입력했습니다.`);
-    // }
   }
+  console.log(users);
+  // const strUsers = JSON.stringify(users);
+  // console.log(strUsers);
+  const testDuplicate = new Set(users.map(JSON.stringify));
+  console.log(testDuplicate);
+  const isDuplicate = testDuplicate.size < users.length;
+  console.log(isDuplicate);
 }
 
 joinBtn[0].addEventListener("click", handleSubmitId);
