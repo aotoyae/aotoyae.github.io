c0b5d93fbe9fcf11e669dca93023cfe869668643
kimsohyeon <aotoyae@gmail.com>
2023-11-04 01:51:46 +0900

    fix: 중복 로직 따로 함수로 분리

--- legal_dong/js/api.js
+++ legal_dong/js/api.js
@@ -3,39 +3,46 @@ const content = document.querySelector("#content");
 const pageList = document.querySelectorAll("#page-list a");
 const pageNumber = document.querySelectorAll("#page-list .num");
 const key = api.key;
+const url = `https://api.odcloud.kr/api/15063424/v1/uddi:257e1510-0eeb-44de-8883-8295c94dadf7?page=1&perPage=10&&serviceKey=${key}`;
 
-fetch(
-  `https://api.odcloud.kr/api/15063424/v1/uddi:257e1510-0eeb-44de-8883-8295c94dadf7?page=1&perPage=10&&serviceKey=${key}`
-)
+function displayJson(json) {
+  let dongData = json.data;
+  dongData.forEach((ele) => {
+    content.innerHTML += `
+                  <tr>
+                  <td>${ele.법정동코드}</td>
+                  <td>${ele.시도명}</td>
+                  <td>${ele.시군구명}</td>
+                  <td>${ele.읍면동명}</td>
+                  <td>${ele.리명}</td>
+                  <td>${ele.순위}</td>
+                  <td>${ele.생성일자}</td>
+                  <td>${ele.삭제일자}</td>
+                  <td>${ele.과거법정동코드}</td>
+                  </tr>`;
+  });
+}
+
+function catchError(error) {
+  console.log(error);
+  document.querySelector("#content").innerHTML += `
+              <tr>
+                  <td colspan="4">Error occurred.</td>
+              </tr>   
+            `;
+}
+
+fetch(url)
   .then((response) => response.json())
   .then((json) => {
     console.log(json.currentCount);
     console.log(json.totalCount);
     console.log(json.page);
     console.log(json.perPage);
-    let dongData = json.data;
-    dongData.forEach((ele) => {
-      content.innerHTML += `
-                    <tr>
-                    <td>${ele.법정동코드}</td>
-                    <td>${ele.시도명}</td>
-                    <td>${ele.시군구명}</td>
-                    <td>${ele.읍면동명}</td>
-                    <td>${ele.리명}</td>
-                    <td>${ele.순위}</td>
-                    <td>${ele.생성일자}</td>
-                    <td>${ele.삭제일자}</td>
-                    <td>${ele.과거법정동코드}</td>
-                    </tr>`;
-    });
+    displayJson(json);
   })
   .catch((error) => {
-    console.log(error);
-    document.querySelector("#content").innerHTML += `
-            <tr>
-                <td colspan="4">Error occurred.</td>
-            </tr>   
-          `;
+    catchError(error);
   });
 
 function pagingNum(page) {
@@ -44,29 +51,10 @@ function pagingNum(page) {
   )
     .then((response) => response.json())
     .then((json) => {
-      let dongData = json.data;
-      dongData.forEach((ele) => {
-        content.innerHTML += `
-                      <tr>
-                      <td>${ele.법정동코드}</td>
-                      <td>${ele.시도명}</td>
-                      <td>${ele.시군구명}</td>
-                      <td>${ele.읍면동명}</td>
-                      <td>${ele.리명}</td>
-                      <td>${ele.순위}</td>
-                      <td>${ele.생성일자}</td>
-                      <td>${ele.삭제일자}</td>
-                      <td>${ele.과거법정동코드}</td>
-                      </tr>`;
-      });
+      displayJson(json);
     })
     .catch((error) => {
-      console.log(error);
-      document.querySelector("#content").innerHTML += `
-              <tr>
-                  <td colspan="4">Error occurred.</td>
-              </tr>   
-            `;
+      catchError(error);
     });
 }
 
