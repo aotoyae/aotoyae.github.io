e03144816bd975552db67435f22aad35f773b99e
kimsohyeon <aotoyae@gmail.com>
2023-11-19 19:29:32 +0900

    fix: isValid === false 시에만 알림뜨도록 수정

--- legal_dong/js/login.js
+++ legal_dong/js/login.js
@@ -108,7 +108,7 @@ function handleLogIn(event) {
   if (isValid) {
     alert(`${id}님 반갑습니다.`);
     window.location.href = "sub.html";
-  } else {
+  } else if (isValid === false) {
     alert(`아이디 또는 비밀번호를 잘못 입력했습니다.`);
   }
 }
